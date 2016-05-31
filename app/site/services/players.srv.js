4d2cd29267917a5b3005f4260f149ad1c85552ad
ryanchick <uw.ryanchick@gmail.com>
2016-05-31 11:59:48 -0400

    standard deviations

--- app/site/services/players.srv.js
+++ app/site/services/players.srv.js
@@ -9,19 +9,21 @@
 		getStats();
 
 		//variables
+		self.stdDev;
 
 		//function bindings
 		self.getStats = getStats;
 
+
 		//functions
 		function getStats(){
+	
 			return $http.get('/api/stats')
 				.then (function(res){
 					self.stats = res.data;
 					getAvg(self.stats);
 					return self.stats
-			})
-						 
+			})		 
 		}
 
 		function getAvg (stats){
@@ -33,47 +35,91 @@
 							  C:{ count: 0, totals:{min:0, pts: 0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fg3a:0,fg3p:0,fgm:0,fga:0,fgp:0,ftm:0,fta:0,ftp:0,tov:0}, avg: {min:0, pts: 0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fg3a:0,fg3p:0,fgm:0,fga:0,fgp:0,ftm:0,fta:0,ftp:0,tov:0}},
 							 };
 
-			var count = 0;
+			var stdDev = {
+							PG:{dev:{pts:0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0},stats:{pts: 0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0}},
+							SG:{dev:{pts:0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0},stats:{pts: 0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0}},
+							SF:{dev:{pts:0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0},stats:{pts: 0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0}},
+							PF:{dev:{pts:0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0},stats:{pts: 0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0}},
+							C:{dev:{pts:0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0},stats:{pts: 0,ast:0,reb:0,stl:0,blk:0,fg3m:0,fgp:0,ftp:0,tov:0}}
+						};
+
+			// var count = 0;
 			for (var i = 0; i < stats.length; i++){
 				if (stats[i].min > 20 && stats[i].games_played > 20){
 					var pos = stats[i].position;
-					totalStats[pos].count ++;
-					totalStats[pos].totals.min += stats[i].min;
-					totalStats[pos].totals.pts += stats[i].pts;
-					totalStats[pos].totals.ast += stats[i].ast;
-					totalStats[pos].totals.reb += stats[i].reb;
-					totalStats[pos].totals.stl += stats[i].stl;
-					totalStats[pos].totals.blk += stats[i].blk;
-					totalStats[pos].totals.fg3m += stats[i].fg3m;
-					totalStats[pos].totals.fg3a += stats[i].fg3a;
-					totalStats[pos].totals.fg3p += stats[i].fg3a == 0 ? 0 : stats[i].fg3m/stats[i].fg3a;
-					totalStats[pos].totals.fgm += stats[i].fgm;
-					totalStats[pos].totals.fga += stats[i].fga;
-					totalStats[pos].totals.fgp += stats[i].fga == 0 ? 0 : stats[i].fgm/stats[i].fga;
-					totalStats[pos].totals.ftm += stats[i].ftm;
-					totalStats[pos].totals.fta += stats[i].fta;
-					totalStats[pos].totals.ftp += stats[i].fta == 0 ? 0 : stats[i].ftm/stats[i].fta;
-					totalStats[pos].totals.tov += stats[i].tov;
+					totalStats[pos].count++;
+
+					for(var stat in totalStats[pos].totals){
+						if(stat == 'fg3p'){
+							totalStats[pos].totals.fg3p += stats[i].fg3p = (stats[i].fg3a == 0 ? 0 : stats[i].fg3m/stats[i].fg3a);
+						} else if(stat == 'fgp'){
+							totalStats[pos].totals.fgp += stats[i].fgp = (stats[i].fga == 0 ? 0 : stats[i].fgm/stats[i].fga);
+						} else if(stat == 'ftp'){
+							totalStats[pos].totals.ftp += stats[i].ftp = (stats[i].fta == 0 ? 0 : stats[i].ftm/stats[i].fta);
+						} else {
+							totalStats[pos].totals[stat] += stats[i][stat];
+						}
+					}
+					if(pos == 'SG'){
+						console.log(stats[i].ftp)
+					}
+					
+					for(var stat in stdDev[pos].stats){
+						if(stat == 'pts'||stat == 'ast'||stat == 'reb'||stat == 'stl'||stat == 'blk'||stat == 'fg3m'||stat == 'fgp'||stat == 'ftp'||stat == 'tov'){
+							if(totalStats[pos].count == 1){
+								stdDev[pos].stats[stat+'2'] = 0;
+								stdDev[pos].stats[stat+'K'] = stats[i][stat];
+							}
+							stdDev[pos].stats[stat] += stats[i][stat] - stdDev[pos].stats[stat+'K'];
+							stdDev[pos].stats[stat + '2'] += (stats[i][stat] - stdDev[pos].stats[stat+'K']) * (stats[i][stat] - stdDev[pos].stats[stat+'K']);
+							// stdDev[pos].stats[stat] += stats[i][stat];
+							// stdDev[pos].stats[stat + '2'] += (stats[i][stat]) * (stats[i][stat]);
+						}
+						// console.log(stdDev[pos].stats)
+					}
+
 				}
 			}
+			console.log(stdDev['SG'].stats)
+			console.log(totalStats['SG'].totals.pts)
+
 			for (var i in totalStats){
-				totalStats[i].avg.min = totalStats[i].totals.min/totalStats[i].count;
-				totalStats[i].avg.pts = totalStats[i].totals.pts/totalStats[i].count;
-				totalStats[i].avg.ast = totalStats[i].totals.ast/totalStats[i].count;
-				totalStats[i].avg.reb = totalStats[i].totals.reb/totalStats[i].count;
-				totalStats[i].avg.stl = totalStats[i].totals.stl/totalStats[i].count;
-				totalStats[i].avg.blk = totalStats[i].totals.blk/totalStats[i].count;
-				totalStats[i].avg.fg3m = totalStats[i].totals.fg3m/totalStats[i].count;
-				totalStats[i].avg.fg3a = totalStats[i].totals.fg3a/totalStats[i].count;
-				totalStats[i].avg.fg3p = totalStats[i].totals.fg3p/totalStats[i].count;
-				totalStats[i].avg.fgm = totalStats[i].totals.fgm/totalStats[i].count;
-				totalStats[i].avg.fga = totalStats[i].totals.fga/totalStats[i].count;
-				totalStats[i].avg.fgp = totalStats[i].totals.fgp/totalStats[i].count;
-				totalStats[i].avg.ftm = totalStats[i].totals.ftm/totalStats[i].count;
-				totalStats[i].avg.fta = totalStats[i].totals.fta/totalStats[i].count;
-				totalStats[i].avg.ftp = totalStats[i].totals.ftp/totalStats[i].count;
-				totalStats[i].avg.tov = totalStats[i].totals.tov/totalStats[i].count;
+				for(var stat in totalStats[i].totals){
+					totalStats[i].avg[stat] = totalStats[i].totals[stat]/totalStats[i].count;
+				}
 			}
+			for (var pos in stdDev){
+				for(var stat in stdDev[pos].dev){
+					if(pos == 'SG' && stat == 'ftp'){
+						console.log(stdDev[pos].stats[stat])
+						console.log(stdDev[pos].stats[stat+'2'])
+						console.log(totalStats)
+					}
+					// console.log(stat)
+					stdDev[pos].dev[stat] = Math.sqrt((stdDev[pos].stats[stat+'2'] - (stdDev[pos].stats[stat] * stdDev[pos].stats[stat])/totalStats[pos].count)/totalStats[pos].count)
+					if(pos == 'SG' && stat == 'ftp'){
+						console.log((stdDev[pos].stats[stat+'2'] - (stdDev[pos].stats[stat] * stdDev[pos].stats[stat])/totalStats[pos].count)/totalStats[pos].count)
+					}
+				}
+			}
+			console.log(totalStats['SG'].avg)
+			console.log(stdDev['SG'].dev)
+
+			/*
+			if len(data) == 0:
+			  return 0
+			K = data[0]
+			n = 0
+			sum_ = 0
+			sum_sqr = 0
+			for x in data:
+			  n = n + 1
+			  sum_ += x - K
+			  sum_sqr += (x - K) * (x - K)
+			variance = (sum_sqr - (sum_ * sum_)/n)/(n - 1)
+			*/
+
+			self.stdDev = stdDev;
 
 			self.avgStats = {
 				PG: totalStats.PG.avg,
