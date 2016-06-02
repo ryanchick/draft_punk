a0fcc46a370581c68011cd31f9b2d1e8b62d7ef8
ryanchick <uw.ryanchick@gmail.com>
2016-06-02 00:36:11 -0400

    draft animations

--- api/routes/draft_routes.js
+++ api/routes/draft_routes.js
@@ -94,35 +94,22 @@ router.get('/players/:leagueId', function(req, res) {
 });
 
 router.post('/', function(req,res){
-	var player = req.body;
+	var player = req.body.player;
 	console.log(player.name)
+
+	//remove player from list
 	var l = availPlayers.length;
 	for(var i = 0; i < l; i++){
 		if(availPlayers[i].nba_id == player.nba_id){
 			console.log('drafted')
 			console.log(i)
 			availPlayers.splice(i,1);
-			i = l;
-			// console.log('newavail')
-			// console.log()
+			break;
 		}
 	}
-	// console.log(availPlayers)
-	res.json(availPlayers)
-})
 
-//get suggestions for team
-router.put('/:teamId', function(req,res){
-	//get players in team
-	//analyze team composition/stats
-	//change weightings
-	//get list of remaining players
-	//return top 3 suggested players
-
-	// console.log(req.params.teamId)
-	// console.log(req.body)
 	var missing = "";
-	var userTeam = req.body.stats;
+	var userTeam = req.body.team.stats;
 
 	var __team = {
 		pts: userTeam.pts/userTeam.count,
@@ -156,12 +143,9 @@ router.put('/:teamId', function(req,res){
 				}
 			}
 		}
-			
 	}
+	console.log('weightings')
 	console.log(teamWgts)
-	// Math.abs(A) < Math.abs(B) ? A : B;
-
-
 
 	var l = availPlayers.length;
 	//clone players array so list of players doesn't change when recalculating ratings
@@ -183,6 +167,8 @@ router.put('/:teamId', function(req,res){
 			__player.tov * teamWgts.tov * ovrWgts.tov
 			* (1 + ((__player.games_played - 72)/ 82));
 	}
+
+	//remove players with invalid ratings or that don't match required position
 	var validPlayers = __newPlayers.filter(function(player){
 		// console.log(player)
 		if(isNaN(player.totRtg)){
@@ -194,20 +180,117 @@ router.put('/:teamId', function(req,res){
 			}
 		}
 		return true;
-		// return true;
 	})
-	// console.log(validPlayers)
+
+	// sort players to find recommendations
 	validPlayers.sort(function(a, b) {
 	  return b.totRtg - a.totRtg;
 	});
-
-	// console.log(validPlayers[0])
 	var suggested = [validPlayers[0],validPlayers[1],validPlayers[2]]
-	// console.log(availPlayers[0].games_played)
-	// console.log(availPlayers[1].games_played)
-	// console.log(availPlayers[1].totRtg)
-	// console.log(availPlayers[2].games_played)
-	res.send(suggested)
+
+	// res.send(suggested)
+	var ret = {
+		players:availPlayers,
+		suggest:suggested
+	}
+	res.json(ret)
+})
+
+//get suggestions for team
+router.put('/:teamId', function(req,res){
+	//get players in team
+	//analyze team composition/stats
+	//change weightings
+	//get list of remaining players
+	//return top 3 suggested players
+
+	// var missing = "";
+	// var userTeam = req.body.stats;
+
+	// var __team = {
+	// 	pts: userTeam.pts/userTeam.count,
+	// 	ast: userTeam.ast/userTeam.count,
+	// 	reb: userTeam.reb/userTeam.count,
+	// 	stl: userTeam.stl/userTeam.count,
+	// 	blk: userTeam.blk/userTeam.count,
+	// 	fg3: userTeam.fg3m/userTeam.count,
+	// 	fgp: userTeam.fga > 0 ? userTeam.fgm/userTeam.fga : 0,
+	// 	ftp: userTeam.fta > 0 ? userTeam.ftm/userTeam.fta : 0,
+	// 	tov: userTeam.tov/userTeam.count
+	// }
+	// var teamWgts = {pts: 1,ast: 1,reb: 1,stl: 1,blk: 1,fg3: 1,fgp: 1,ftp: 1,tov: 1}
+
+	// //adjust team stat weightings based on current stats
+	// if(__team.pts > 0){
+	// 	for(var stat in __team){
+	// 		// console.log(stat)
+	// 		var __change = (goals[stat]-__team[stat])/Math.min(__team[stat],goals[stat]) * maxVar;
+	// 		var __change = (Math.abs(__change) < maxVar) ? __change : (__change >= 0 ? maxVar : maxVar * -1);
+	// 		teamWgts[stat] = 1 + __change;
+	// 	}
+	// }
+	// if(userTeam.count >= 7){
+	// 	for(var pos in req.body.players){
+	// 		if(!(pos.match('UTIL') || pos.match('BENCH'))){
+	// 			if(req.body.players[pos] == ""){
+	// 				missing = pos;
+	// 				console.log(missing)
+	// 				break;
+	// 			}
+	// 		}
+	// 	}
+			
+	// }
+	// console.log(teamWgts)
+	// // Math.abs(A) < Math.abs(B) ? A : B;
+
+
+
+	// var l = availPlayers.length;
+	// //clone players array so list of players doesn't change when recalculating ratings
+	// var __newPlayers = JSON.parse(JSON.stringify(availPlayers));
+	// for(var i = 0; i < l;i++){
+	// 	var __player = __newPlayers[i];
+	// 	__player.fgp = __player.fga > 0 ? __player.fgm/__player.fga * 100: 0;
+	// 	// console.log(__player.fgp)
+	// 	__player.ftp = __player.fta > 0 ? __player.ftm/__player.fta * 100: 0;
+	// 	__newPlayers[i].totRtg = 
+	// 		__player.pts * teamWgts.pts * ovrWgts.pts +
+	// 		__player.ast * teamWgts.ast * ovrWgts.ast +
+	// 		__player.reb * teamWgts.reb * ovrWgts.reb +
+	// 		__player.stl * teamWgts.stl * ovrWgts.stl +
+	// 		__player.blk * teamWgts.blk * ovrWgts.blk +
+	// 		__player.fg3m * teamWgts.fg3 * ovrWgts.fg3 +
+	// 		(__player.fgp - ovrWgts.fgpAvg) * __player.fga * teamWgts.fgp * ovrWgts.fgp + 
+	// 		(__player.ftp - ovrWgts.ftpAvg) * __player.fta * teamWgts.ftp * ovrWgts.ftp +
+	// 		__player.tov * teamWgts.tov * ovrWgts.tov
+	// 		* (1 + ((__player.games_played - 72)/ 82));
+	// }
+	// var validPlayers = __newPlayers.filter(function(player){
+	// 	// console.log(player)
+	// 	if(isNaN(player.totRtg)){
+	// 		return false;
+	// 	}
+	// 	if(missing != ""){
+	// 		if(!player.position.match(missing)){
+	// 			return false;
+	// 		}
+	// 	}
+	// 	return true;
+	// 	// return true;
+	// })
+	// // console.log(validPlayers)
+	// validPlayers.sort(function(a, b) {
+	//   return b.totRtg - a.totRtg;
+	// });
+
+	// // console.log(validPlayers[0])
+	// var suggested = [validPlayers[0],validPlayers[1],validPlayers[2]]
+	// // console.log(availPlayers[0].games_played)
+	// // console.log(availPlayers[1].games_played)
+	// // console.log(availPlayers[1].totRtg)
+	// // console.log(availPlayers[2].games_played)
+	// res.send(suggested)
 })
 
 
